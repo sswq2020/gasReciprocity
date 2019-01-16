00ec413fdd9bb16a0cf5edc2dab49c6632e9d629
mazheng <maz@hletong.com>
2019-01-16 11:34:28 +0800

    修改了路由

---
+++ src/pages/GasSys/GasList.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react';
+import PageHeaderWrapper from '@/components/PageHeaderWrapper';
+
+class Page extends PureComponent {
+  render() {
+    return <PageHeaderWrapper>gas list</PageHeaderWrapper>;
+  }
+}
+
+export default Page;
