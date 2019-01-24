cb7400549b3039013feccc609b784a3100fc63c5
sswq2020 <a309108243@qq.com>
2019-01-24 17:09:48 +0800

    PriceHistoryList模块完成

--- src/pages/GasStationManage/components/PriceHistoryList.js
+++ src/pages/GasStationManage/components/PriceHistoryList.js
@@ -1,6 +1,7 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import { connect } from 'dva';
 import { Modal } from 'antd';
+import TableList from '@/components/TableList';
 
 @connect(({ priceHistoryList }) => ({
   priceHistoryList,
@@ -9,24 +10,89 @@ class CustomizeComponent extends PureComponent {
   render() {
     const {
       dispatch,
-      priceHistoryList: { id, visible },
+      priceHistoryList: {
+        visible,
+        listParams: { currentPage },
+        list: { list: listData, itemCount: totalItemCount },
+      },
     } = this.props;
+
+    const listProps = {
+      columns: [
+        {
+          title: '油品名称',
+          key: 'fuelName',
+          width: 100,
+          render: (text, record) => <Fragment>{record.fuelName}</Fragment>,
+        },
+        {
+          title: '零售价',
+          key: 'retailPrice',
+          width: 100,
+          render: (text, record) => <Fragment>￥{parseFloat(record.b).toFixed(2)}</Fragment>,
+        },
+        {
+          title: '会员折扣(%)',
+          key: 'memberDiscount',
+          width: 100,
+          render: (text, record) => <Fragment>{record.c}%</Fragment>,
+        },
+        {
+          title: '会员价',
+          key: 'memberPrice',
+          width: 100,
+          render: (text, record) => <Fragment>￥{parseFloat(record.d).toFixed(2)}</Fragment>,
+        },
+        {
+          title: '生效时间',
+          key: 'effectTime',
+          width: 120,
+          render: (text, record) => <Fragment>￥{record.f}</Fragment>,
+        },
+        {
+          title: '调价申请时间',
+          key: 'HisTime',
+          width: 120,
+          render: (text, record) => <Fragment>￥{record.f}</Fragment>,
+        },
+      ],
+      rowKey: 'id',
+      scroll: { x: 'max-content' },
+      dataSource: listData,
+      style: {
+        marginTop: 24,
+      },
+      pagination: {
+        total: totalItemCount,
+        current: currentPage,
+      },
+      onChange: pagination => {
+        dispatch({
+          type: 'priceApply/changeListParams',
+          payload: {
+            pagcurrentPagee: pagination.current,
+          },
+        });
+      },
+    };
+
     return (
       <Modal
-        title="hello"
+        width={1000}
+        title="调价历史"
         visible={visible}
         onOk={() => {
           dispatch({
             type: 'priceHistoryList/closePopup',
           });
         }}
-        onClose={() => {
+        onCancel={() => {
           dispatch({
             type: 'priceHistoryList/closePopup',
           });
         }}
       >
-        {id}
+        <TableList {...listProps} />
       </Modal>
     );
   }
